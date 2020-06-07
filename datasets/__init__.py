9fdc6f11f1ac5ffb45044e2621fbe1b6652f98fa
haleqiu <115010216@link.cuhk.edu.cn>
2020-06-07 19:06:40 -0400

    fix name and class

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,4 +1,4 @@
 from .datasets import Citation
 from .continuum import Continuum
 from .continuum import citation_collate
-from .continumm_reddit import Continuum_reddit
+from .continummLS import ContinuumLS
