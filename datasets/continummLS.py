63fdb46ab6fef249cf877da6b1ccf237d399b1d7
haleqiu <115010216@link.cuhk.edu.cn>
2020-06-08 01:59:16 -0400

    fix

--- datasets/continummLS.py
+++ datasets/continummLS.py
@@ -43,7 +43,7 @@ class ContinuumLS(VisionDataset):
 
     def __getitem__(self, index):
         #train all may have no neiborugh
-        neighbor_mask = np.append(self.adj_full[self.mask[0]].nonzero()[1],index)
+        neighbor_mask = np.append(self.adj_full[self.mask[index]].nonzero()[1],index)
         neighbor = self.features[neighbor_mask]
         return torch.FloatTensor(self.features[self.mask[index]]).unsqueeze(-2), self.labels[self.mask[index]], torch.FloatTensor(neighbor).unsqueeze(-2)
 
