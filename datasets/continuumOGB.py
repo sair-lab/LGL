419062e5d4710b35f7e148011d4a5da251e198e7
Yuheng <yuhengq@andrew.cmu.edu>
2020-11-22 01:15:30 +0000

    fix ogb dataset __len__; torch.logitand

--- datasets/continuumOGB.py
+++ datasets/continuumOGB.py
@@ -10,31 +10,31 @@ from ogb.nodeproppred import NodePropPredDataset
 
 
 class ContinuumOGB(VisionDataset):
-    def __init__(self, root='~/.dgl', name='cora', data_type='train', download=True, task_type=0):
+    def __init__(self, root='~/.dgl', name='"ogbn-arxiv"', data_type='train', download=True, task_type=0):
         super(ContinuumOGB, self).__init__(root)
         self.name = name
 
         self.download()
         self.features = torch.FloatTensor(self.data['node_feat'])
-        self.num_nodes = len(self.labels)
+
         ## add self loop
-        self_loop = torch.LongTensor(list(range(self.num_nodes)))
+        self_loop = torch.LongTensor(list(range(len(self.labels))))
         self.src, self.dst = torch.LongTensor(self.data["edge_index"])
         self.src = torch.cat((self.src, self_loop), 0)
         self.dst = torch.cat((self.dst, self_loop), 0)
 
         if data_type == 'incremental':
             mask = self.idx_split[data_type]
-            self.mask = (np.logical_and((self.labels==task_type),mask)).type(torch.bool)
-        elif data_type in ['train','test','valid']: # class incremental; use test and train as train
-            self.mask = torch.LongTensor(self.idx_split[data_type]) # use val as test, since val is larger than test
+            self.mask = (torch.logical_and((self.labels==task_type),mask)).type(torch.bool)
+        elif data_type in ['train','test','valid']:
+            self.mask = torch.LongTensor(self.idx_split[data_type])
         else:
             raise RuntimeError('data type {} wrong'.format(data_type))
 
         print('{} Dataset for {} Loaded.'.format(self.name, data_type))
 
     def __len__(self):
-        return self.num_nodes
+        return len(self.labels[self.mask])
 
     def __getitem__(self, index):
         neighbor = self.features[self.dst[self.src==self.mask[index]]]
