d6e78947ebfc0524b0dd35b1628a4e9849014815
Yuheng <yuhengq@andrew.cmu.edu>
2021-05-21 03:00:55 +0000

    update non lifelong training setup

--- non_lifelong.py
+++ non_lifelong.py
@@ -36,13 +36,16 @@ import torch.nn as nn
 import torch.utils.data as Data
 from torch.autograd import Variable
 
-from models import SAGE
-from models import KLGL, LGL
-from models import KCAT, KTransCAT
+from models import SAGE, GCN, APPNP, MLP, GAT
+from models import LGL, AFGN, PlainNet, AttnPlainNet
+from models import KTransCAT
 from lifelong import performance
 from datasets import continuum, graph_collate
 from torch_util import count_parameters, EarlyStopScheduler
+import time 
 
+## AFGN is LGL with attention; AttnPlainNet is the PlainNet with attention
+nets = {'sage':SAGE, 'lgl': LGL, 'ktranscat':KTransCAT, 'gcn':GCN, 'appnp':APPNP, 'mlp':MLP, 'gat':GAT, 'afgn':AFGN, 'plain':PlainNet, 'attnplain':AttnPlainNet}
 
 def performance(loader, net, device):
     net.eval()
@@ -50,7 +53,12 @@ def performance(loader, net, device):
     with torch.no_grad():
         for batch_idx, (inputs, targets, neighbor) in enumerate(tqdm.tqdm(loader)):
             if torch.cuda.is_available():
-                inputs, targets, neighbor = inputs.to(device), targets.to(device), [[element.to(device) for element in item]for item in neighbor]
+                inputs, targets = inputs.to(device), targets.to(device)
+                if not args.k:
+                    neighbor = [element.to(device) for element in neighbor]
+                else:
+                    neighbor = [[element.to(device) for element in item]for item in neighbor]
+
             outputs = net(inputs, neighbor)
             _, predicted = torch.max(outputs.data, 1)
             total += targets.size(0)
@@ -62,10 +70,13 @@ def train(loader, net, criterion, optimizer, device):
     net.train()
     train_loss, correct, total = 0, 0, 0
     for batch_idx, (inputs, targets, neighbor) in enumerate(tqdm.tqdm(loader)):
-        inputs, targets, neighbor = inputs.to(device), targets.to(device), [[element.to(device) for element in item]for item in neighbor]
+        inputs, targets = inputs.to(device), targets.to(device)
+        if not args.k:
+            neighbor = [element.to(device) for element in neighbor]
+        else:
+            neighbor = [[element.to(device) for element in item]for item in neighbor]
 
         optimizer.zero_grad()
-
         outputs = net(inputs, neighbor)
 
         loss = criterion(outputs, targets)
@@ -92,17 +103,19 @@ if __name__ == '__main__':
     parser.add_argument("--optm", type=str, default='SGD', help="SGD or Adam")
     parser.add_argument("--lr", type=float, default=0.01, help="learning rate")
     parser.add_argument("--factor", type=float, default=0.1, help="ReduceLROnPlateau factor")
-    parser.add_argument("--min-lr", type=float, default=0.01, help="minimum lr for ReduceLROnPlateau")
+    parser.add_argument("--min-lr", type=float, default=0.001, help="minimum lr for ReduceLROnPlateau")
     parser.add_argument("--patience", type=int, default=5, help="patience for Early Stop")
     parser.add_argument("--batch-size", type=int, default=10, help="number of minibatch size")
     parser.add_argument("--milestones", type=int, default=15, help="milestones for applying multiplier")
     parser.add_argument("--epochs", type=int, default=20, help="number of training epochs")
     parser.add_argument("--early-stop", type=int, default=5, help="number of epochs for early stop training")
-    parser.add_argument("--momentum", type=float, default=0, help="momentum of the optimizer")
+    parser.add_argument("--momentum", type=float, default=0.9, help="momentum of the optimizer")
     parser.add_argument("--gamma", type=float, default=0.1, help="learning rate multiplier")
     parser.add_argument("--seed", type=int, default=0, help='Random seed.')
     parser.add_argument("--eval", type=str, default=None, help="the path to eval the acc")
-    parser.add_argument("--k", type=int, default=1, help='Random seed.')
+    parser.add_argument("--k", type=int, default=None, help='khop.')
+    parser. add_argument("--hidden", type=int, nargs="+", default=[64,32])
+    parser. add_argument("--drop", type=float, nargs="+", default=[0,0])
     args = parser.parse_args(); print(args)
     torch.manual_seed(args.seed)
 
@@ -120,15 +133,15 @@ if __name__ == '__main__':
         print("Train Acc: %.5f, Test Acc: %.5f, Valid Acc: %.5f"%(train_acc, test_acc, valid_acc))
         exit()
     else:
-        print(args.device)
-        nets = {'klgl':KLGL,'sage':SAGE, 'lgl': LGL, 'kcat': KCAT, 'ktranscat':KTransCAT}
         Net = nets[args.model.lower()]
         
-        ## TODO the sage and lgl should also include k 
-        if args.model.lower() == 'sage' or args.model.lower() == 'lgl':
-            net = Net(feat_len=train_data.feat_len, num_class=train_data.num_class).to(args.device)
-        else:
+        if args.model.lower() == 'ktranscat':
             net = Net(feat_len=train_data.feat_len, k=args.k, num_class=train_data.num_class).to(args.device)
+        else:
+            assert(args.k == None)
+            net = Net(feat_len=train_data.feat_len, num_class=train_data.num_class, hidden = args.hidden, dropout = args.drop).to(args.device)
+
+    print(net)
 
     criterion = nn.CrossEntropyLoss()
     exec('optimizer = torch.optim.%s(net.parameters(), lr=%f)'%(args.optm, args.lr))
