47d9df5b2b70a99007ca1b4590ab32fc4f104fd8
Chen Wang <jeffsan@gmx.com>
2021-05-22 23:17:45 -0400

    update script

--- scripts/ClassLifelong/lifelongclass.sh
+++ scripts/ClassLifelong/lifelongclass.sh
@@ -13,19 +13,28 @@ BATCH=6
 DATASET=citeseer
 for SEED in 1 2 3
 do
-    python lifelong.py --save loads/${MODEL}_class_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${SEED}.model --eval ./Exp_lifelongclass/${MODEL}/${MODEL}_class_${DATASET}_J${JUMP} --data-root ./data --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --merge ${MERGE} --hidden 10 10 --drop 0 0
+    python lifelong.py --save loads/${MODEL}_class_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${SEED}.model \
+        --eval ./Exp_lifelongclass/${MODEL}/${MODEL}_class_${DATASET}_J${JUMP} --data-root ./data --dataset ${DATASET} \
+        --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} \
+        --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --merge ${MERGE} --hidden 10 10 --drop 0 0
 done
 
 DATASET=pubmed
 BATCH=10
 for SEED in 1 2 3
 do
-    python lifelong.py --save loads/${MODEL}_class_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${SEED}.model --eval ./Exp_lifelongclass/${MODEL}/${MODEL}_class_${DATASET}_J${JUMP} --data-root ./data --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --merge ${MERGE} --hidden 10 10 --drop 0 0
+    python lifelong.py --save loads/${MODEL}_class_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${SEED}.model \
+        --eval ./Exp_lifelongclass/${MODEL}/${MODEL}_class_${DATASET}_J${JUMP} --data-root ./data --dataset ${DATASET} \
+        --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} \
+        --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --merge ${MERGE} --hidden 10 10 --drop 0 0
 done
 
 DATASET=cora
 BATCH=10
 for SEED in 1 2 3
 do
-    python lifelong.py --save loads/${MODEL}_class_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${SEED}.model --eval ./Exp_lifelongclass/${MODEL}/${MODEL}_class_${DATASET}_J${JUMP} --data-root ./data --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --merge ${MERGE} --hidden 10 10 --drop 0 0
+    python lifelong.py --save loads/${MODEL}_class_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${SEED}.model \
+        --eval ./Exp_lifelongclass/${MODEL}/${MODEL}_class_${DATASET}_J${JUMP} --data-root ./data --dataset ${DATASET} \
+        --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} \
+        --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --merge ${MERGE} --hidden 10 10 --drop 0 0
 done
--- scripts/DataLifelong/lifelongdata.sh
+++ scripts/DataLifelong/lifelongdata.sh
@@ -12,19 +12,28 @@ DATASET=citeseer
 BATCH=6
 for SEED in 1 2 3
 do
-    python lifelong_data.py --save loads/${MODEL}_data_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${OPT}_${SEED}.model --eval ./Exp_lifelongdata/${MODEL}/${MODEL}_data_${DATASET}_J${JUMP}_${OPT} --data-root ./data --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --hidden 10 10 --drop 0 0
+    python lifelong_data.py --save loads/${MODEL}_data_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${OPT}_${SEED}.model \
+        --eval ./Exp_lifelongdata/${MODEL}/${MODEL}_data_${DATASET}_J${JUMP}_${OPT} --data-root ./data \
+        --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE \
+        --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --hidden 10 10 --drop 0 0
 done
 
 BATCH=10
 DATASET=pubmed
 for SEED in 1 2 3
 do
-    python lifelong_data.py --save loads/${MODEL}_data_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${OPT}_${SEED}.model --eval ./Exp_lifelongdata/${MODEL}/${MODEL}_data_${DATASET}_J${JUMP}_${OPT} --data-root ./data --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --hidden 10 10 --drop 0 0
+    python lifelong_data.py --save loads/${MODEL}_data_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${OPT}_${SEED}.model \
+        --eval ./Exp_lifelongdata/${MODEL}/${MODEL}_data_${DATASET}_J${JUMP}_${OPT} --data-root ./data\
+        --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE \
+        --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --hidden 10 10 --drop 0 0
 done
 
 
 DATASET=cora
 for SEED in 1 2 3
 do
-    python lifelong_data.py --save loads/${MODEL}_data_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${OPT}_${SEED}.model --eval ./Exp_lifelongdata/${MODEL}/${MODEL}_data_${DATASET}_J${JUMP}_${OPT} --data-root ./data --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --hidden 10 10 --drop 0 0
+    python lifelong_data.py --save loads/${MODEL}_data_${DATASET}_${OPT}_${MEMORY}_J${JUMP}_${OPT}_${SEED}.model \
+        --eval ./Exp_lifelongdata/${MODEL}/${MODEL}_data_${DATASET}_J${JUMP}_${OPT} --data-root ./data \
+        --dataset ${DATASET} --lr 0.01 --batch-size ${BATCH} --iteration 5 --memory ${MEMORY} --device $DEVICE \
+        --sample-rate ${SAMPLE} --seed $SEED --model ${MODEL} --jump ${JUMP} --optm ${OPT} --hidden 10 10 --drop 0 0
 done
